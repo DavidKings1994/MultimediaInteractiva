57b8e5360f5eac92e50cd0e586a6d5a09a3e3a52
David Reyes <david.reyes.1994@hotmail.com>
2016-07-05 00:06:43 -0500

    setup inicial

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>Mulimetia Interactiva</title>
+    <script type="text/javascript" src="js/dist/main.bundle.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <style>
+			body { margin: 0; }
+			canvas { width: 100%; height: 100% }
+		</style>
 </head>
 <body>
 </body>
